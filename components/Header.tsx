8f1fd7dca774061bb030c8260fbec9e925032f89
twsau <contact.twsau@gmail.com>
2023-11-11 16:19:08 +0000

    delete all tasks

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
-import { HamburgerMenuIcon, PlusIcon } from "@radix-ui/react-icons";
+import { HamburgerMenuIcon, PlusIcon, TrashIcon } from "@radix-ui/react-icons";
 import { addTask } from "@/state/actions/addTask";
 import {
   Sheet,
@@ -13,6 +13,17 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "./ui/sheet";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@radix-ui/react-alert-dialog";
+import { AlertDialogFooter, AlertDialogHeader } from "./ui/alert-dialog";
+import { setTasks } from "@/state/actions/setTasks";
 
 export const Header: React.FC = () => {
   const [newTaskText, setNewTaskText] = useState("");
@@ -30,6 +41,30 @@ export const Header: React.FC = () => {
             <SheetTitle>task-roulette</SheetTitle>
             <SheetDescription>Menu</SheetDescription>
           </SheetHeader>
+          <div className="py-5">
+            <AlertDialog>
+              <AlertDialogTrigger asChild>
+                <Button className="flex w-full justify-between" variant="ghost">
+                  Delete all tasks
+                  <TrashIcon className="text-destructive" />
+                </Button>
+              </AlertDialogTrigger>
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                  <AlertDialogDescription>
+                    This action cannot be undone.
+                  </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogCancel>Cancel</AlertDialogCancel>
+                  <AlertDialogAction onClick={() => setTasks([])}>
+                    Continue
+                  </AlertDialogAction>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
+          </div>
         </SheetContent>
       </Sheet>
       <form className="flex w-full items-center gap-3">
