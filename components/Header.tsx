40a7de773bfbe1430c8458a00882f47aaaba1533
twsau <contact.twsau@gmail.com>
2023-11-11 13:55:52 +0000

    menu sidebar

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,15 +3,36 @@
 import { useState } from "react";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
-import { PlusIcon } from "@radix-ui/react-icons";
+import { HamburgerMenuIcon, PlusIcon } from "@radix-ui/react-icons";
 import { addTask } from "@/state/actions/addTask";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
 
 export const Header: React.FC = () => {
   const [newTaskText, setNewTaskText] = useState("");
 
   return (
-    <header className="p-5">
-      <form className="flex items-center gap-5">
+    <header className="flex items-center gap-5 p-5">
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button variant="outline">
+            <HamburgerMenuIcon />
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left">
+          <SheetHeader>
+            <SheetTitle>task-roulette</SheetTitle>
+            <SheetDescription>Menu</SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+      <form className="flex w-full items-center gap-5">
         <Input
           className="h-[42px]"
           onChange={(e) => setNewTaskText(e.target.value)}
