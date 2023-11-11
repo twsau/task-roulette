5783dae4f7eb4effde4935deedcf38133d5db679
twsau <contact.twsau@gmail.com>
2023-11-11 04:05:19 +0000

    clear text on addTask

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { useState } from "react";
-import { Input } from "../ui/input";
-import { Button } from "../ui/button";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
 import { PlusIcon } from "@radix-ui/react-icons";
 import { addTask } from "@/state/actions/addTask";
 
@@ -16,7 +16,12 @@ export const Header: React.FC = () => {
         placeholder="Do the things..."
         value={newTaskText}
       />
-      <Button onClick={() => addTask(newTaskText)}>
+      <Button
+        onClick={() => {
+          addTask(newTaskText);
+          setNewTaskText("");
+        }}
+      >
         <PlusIcon />
       </Button>
     </header>
