6bbd62a955f6b754f3413c10512876f2997a3961
twsau <contact.twsau@gmail.com>
2023-11-12 18:16:11 +0000

    layout tweak

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Header } from "@/components/Header";
 import { Hydrate } from "./Hydrate";
-import { Footer } from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,9 +18,7 @@ export default function RootLayout({
   return (
     <html className="dark" lang="en">
       <body className={inter.className}>
-        <Header />
         {children}
-        <Footer />
         <Hydrate />
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import { TaskList } from "@/components/TaskList";
+import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
 
 export default function Home() {
   return (
     <>
+      <Header />
       <TaskList />
+      <Footer />
     </>
   );
 }
